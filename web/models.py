3076107ea1696fc29064c242193fbc9a301d4e60
jadsongmatos <jadson.g-matos@outlook.com>
2023-10-19 20:02:34 -0300

    model

--- web/models.py
+++ web/models.py
@@ -1,30 +1,3 @@
-"""
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    nova_coluna = models.CharField(max_length=200, null=True, blank=True)
-    
-    # Altere os related_names aqui
-    groups = models.ManyToManyField(
-        'auth.Group',
-        verbose_name=('groups'),
-        blank=True,
-        help_text=('The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
-        related_name="customuser_set",  # related_name personalizado
-        related_query_name="customuser",
-    )
-
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        verbose_name=('user permissions'),
-        blank=True,
-        help_text=('Specific permissions for this user.'),
-        related_name="customuser_set",  # related_name personalizado
-        related_query_name="customuser",
-    )
-"""
-
 from django.db import models
 import uuid
 
